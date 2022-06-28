83b38f8f1f95d6f4908472c668df0b2f18aab0f4
darkuserb <103330740+darkuserb@users.noreply.github.com>
2022-06-28 15:33:43 +0400

    Update __main__.py

--- dark_installer/__main__.py
+++ dark_installer/__main__.py
@@ -1,6 +1,6 @@
 from time import time
 from git import Repo
-from dark_nstaller import *
+from dark_installer import *
 from .astring import main
 from telethon import TelegramClient, functions
 from telethon.sessions import StringSession
